03303317c528304f28864fdce618526415a40363
Youssef Khaled <109810438+youssef869@users.noreply.github.com>
2024-10-23 19:19:18 +0300

    Delete low_power_system directory

--- low_power_system/run.do
+++
@@ -1,5 +0,0 @@
-vlib work
-vlog *.*v
-vsim -voptargs=+acc work.SYS_TOP_TB
-do wave.do
-run -all
--- low_power_system/start_check.sv
+++
@@ -1,9 +0,0 @@
-module start_check(
-input  wire start_bit,
-input  wire start_check_en,
-output wire start_glitch
-);
-
-assign start_glitch = (start_check_en && start_bit != 1'b0);
-
-endmodule
